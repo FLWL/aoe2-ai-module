a20d432a89a4c14df2d26aa46a5c5c2160dccc28
FLWL <12775045+FLWL@users.noreply.github.com>
2020-10-11 17:25:22 +0300

    Allow use of standard protobuf include files

--- protos/make-protos.bat
+++ protos/make-protos.bat
@@ -1,23 +1,27 @@
+@echo off
 cd ..
-set protoc_path=vcpkg/installed/x64-windows/tools/protobuf/protoc.exe
-set grpc_cpp_path=vcpkg/installed/x64-windows/tools/grpc/grpc_cpp_plugin.exe
-set grpc_python_path=vcpkg/installed/x64-windows/tools/grpc/grpc_python_plugin.exe
+set protoc_path=%cd%/vcpkg/installed/x64-windows/tools/protobuf/protoc.exe
+set grpc_cpp_path=%cd%/vcpkg/installed/x64-windows/tools/grpc/grpc_cpp_plugin.exe
+set grpc_python_path=%cd%/vcpkg/installed/x64-windows/tools/grpc/grpc_python_plugin.exe
+set standard_include_path=%cd%/vcpkg/installed/x64-windows-static/include
 
 :: cpp protobuf files
-%cd%/%protoc_path% --cpp_out aimodule/ protos/ai_module_api.proto
-%cd%/%protoc_path% --cpp_out aimodule/ protos/expert/expert_api.proto
-%cd%/%protoc_path% --cpp_out aimodule/ protos/expert/action/action.proto
-%cd%/%protoc_path% --cpp_out aimodule/ protos/expert/fact/fact.proto
+%protoc_path% -I . -I %standard_include_path% --cpp_out aimodule/ protos/ai_module_api.proto
+%protoc_path% -I . -I %standard_include_path% --cpp_out aimodule/ protos/expert/expert_api.proto
+%protoc_path% -I . -I %standard_include_path% --cpp_out aimodule/ protos/expert/action/action.proto
+%protoc_path% -I . -I %standard_include_path% --cpp_out aimodule/ protos/expert/fact/fact.proto
 :: cpp grpc files
-%cd%/%protoc_path% -I . --grpc_out aimodule/ --plugin=protoc-gen-grpc=%cd%/%grpc_cpp_path% protos/ai_module_api.proto
-%cd%/%protoc_path% -I . --grpc_out aimodule/ --plugin=protoc-gen-grpc=%cd%/%grpc_cpp_path% protos/expert/expert_api.proto
+%protoc_path% -I . -I %standard_include_path% --grpc_out aimodule/ --plugin=protoc-gen-grpc=%grpc_cpp_path% protos/ai_module_api.proto
+%protoc_path% -I . -I %standard_include_path% --grpc_out aimodule/ --plugin=protoc-gen-grpc=%grpc_cpp_path% protos/expert/expert_api.proto
 
 :: Python protobuf files
-%cd%/%protoc_path% --python_out client_python/ protos/ai_module_api.proto
-%cd%/%protoc_path% --python_out client_python/ protos/expert/expert_api.proto
-%cd%/%protoc_path% --python_out client_python/ protos/expert/action/action.proto
-%cd%/%protoc_path% --python_out client_python/ protos/expert/fact/fact.proto
+%protoc_path% -I . -I %standard_include_path% --python_out client_python/ protos/ai_module_api.proto
+%protoc_path% -I . -I %standard_include_path% --python_out client_python/ protos/expert/expert_api.proto
+%protoc_path% -I . -I %standard_include_path% --python_out client_python/ protos/expert/action/action.proto
+%protoc_path% -I . -I %standard_include_path% --python_out client_python/ protos/expert/fact/fact.proto
 :: Python grpc files
-%cd%/%protoc_path% -I . --grpc_out client_python/ --plugin=protoc-gen-grpc=%cd%/%grpc_python_path% protos/ai_module_api.proto
-%cd%/%protoc_path% -I . --grpc_out client_python/ --plugin=protoc-gen-grpc=%cd%/%grpc_python_path% protos/expert/expert_api.proto
+%protoc_path% -I . -I %standard_include_path% --grpc_out client_python/ --plugin=protoc-gen-grpc=%grpc_python_path% protos/ai_module_api.proto
+%protoc_path% -I . -I %standard_include_path% --grpc_out client_python/ --plugin=protoc-gen-grpc=%grpc_python_path% protos/expert/expert_api.proto
+
+echo All done
 pause
