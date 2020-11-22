515271bd35273bc5194a50dfaacf4d9b2b4234eb
FLWL <12775045+FLWL@users.noreply.github.com>
2020-11-22 23:06:40 +0200

    Add memory utility functions

--- aimodule/misc/Statics.h
+++ aimodule/misc/Statics.h
@@ -1,5 +1,8 @@
 #pragma once
 #include "misc/Configuration.h"
+
+#include <vector>
+
 #include <Windows.h>
 
 namespace statics
@@ -19,4 +22,45 @@ namespace statics
 	{
 		*reinterpret_cast<uintptr_t*>(&func) = newAddr;
 	}
+
+#if defined GAME_AOC
+	static void PlaceJumpInstruction(uint8_t* addr, uint8_t* target, size_t length, uint8_t* replacedBytes = nullptr)
+	{
+		// make memory accessible
+		DWORD oldProtect;
+		VirtualProtect(addr, length, PAGE_EXECUTE_READWRITE, &oldProtect);
+
+		// copy original bytes in case we want to restore them later
+		if (replacedBytes != nullptr)
+			memcpy(replacedBytes, addr, length);
+
+		// put a jmp instruction at the desired location
+		*(addr + 0) = 0xE9;
+
+		// calculate and set the jmp instruction target
+		*(uintptr_t*)(addr + 1) = (uintptr_t)target - (uintptr_t)addr - 5;
+
+		// pad any leftover bytes with nops
+		for (size_t i = 5; i < length; i++)
+			*(addr + i) = 0x90;
+
+		// restore memory protection flags
+		DWORD oldProtectTemp;
+		VirtualProtect((PVOID)addr, length, oldProtect, &oldProtectTemp);
+	}
+
+	static void WriteMemory(uint8_t* addr, uint8_t* bytes, size_t size)
+	{
+		// make memory accessible
+		DWORD oldProtect;
+		VirtualProtect(addr, size, PAGE_EXECUTE_READWRITE, &oldProtect);
+
+		// copy
+		memcpy(addr, bytes, size);
+
+		// restore memory protection flags
+		DWORD oldProtectTemp;
+		VirtualProtect((PVOID)addr, size, oldProtect, &oldProtectTemp);
+	}
+#endif
 }
