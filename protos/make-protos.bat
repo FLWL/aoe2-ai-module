d04e1e42c446c47903e0e122f406ad50726c2a6b
FLWL <12775045+FLWL@users.noreply.github.com>
2021-03-17 20:41:56 +0200

    Add new command.proto to build list.

--- protos/make-protos.bat
+++ protos/make-protos.bat
@@ -10,6 +10,7 @@ echo Generating AIModule C++ protobuf includes
 %protoc_path% -I %workspace_path% -I %standard_include_path% --cpp_out %workspace_path%\aimodule\ protos/ai_module_api.proto
 %protoc_path% -I %workspace_path% -I %standard_include_path% --cpp_out %workspace_path%\aimodule\ protos/expert/expert_api.proto
 %protoc_path% -I %workspace_path% -I %standard_include_path% --cpp_out %workspace_path%\aimodule\ protos/expert/action/action.proto
+%protoc_path% -I %workspace_path% -I %standard_include_path% --cpp_out %workspace_path%\aimodule\ protos/expert/command/command.proto
 %protoc_path% -I %workspace_path% -I %standard_include_path% --cpp_out %workspace_path%\aimodule\ protos/expert/fact/fact.proto
 echo Generating AIModule C++ grpc includes
 %protoc_path% -I %workspace_path% -I %standard_include_path% --grpc_out %workspace_path%\aimodule\ --plugin=protoc-gen-grpc=%grpc_cpp_path% protos/ai_module_api.proto
@@ -19,6 +20,7 @@ echo Generating AIModule Python protobuf includes
 %protoc_path% -I %workspace_path% -I %standard_include_path% --python_out %workspace_path%\client_python\ protos/ai_module_api.proto
 %protoc_path% -I %workspace_path% -I %standard_include_path% --python_out %workspace_path%\client_python\ protos/expert/expert_api.proto
 %protoc_path% -I %workspace_path% -I %standard_include_path% --python_out %workspace_path%\client_python\ protos/expert/action/action.proto
+%protoc_path% -I %workspace_path% -I %standard_include_path% --python_out %workspace_path%\client_python\ protos/expert/command/command.proto
 %protoc_path% -I %workspace_path% -I %standard_include_path% --python_out %workspace_path%\client_python\ protos/expert/fact/fact.proto
 echo Generating AIModule Python grpc includes
 %protoc_path% -I %workspace_path% -I %standard_include_path% --grpc_out %workspace_path%\client_python\ --plugin=protoc-gen-grpc=%grpc_python_path% protos/ai_module_api.proto
